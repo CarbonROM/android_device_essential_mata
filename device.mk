af0dbb61e8d099f198881f0f62a962fc071e5e2a
Anay Wadhera <awadhera@berkeley.edu>
2020-02-19 18:01:46 -0800

    mata: inline with aosp
    * kanged audio options from taimen
    
    Change-Id: I09f9db1070acd2daa91e4526cd38554431a7e18b

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 -include device/essential/mata/system_prop.mk
 -include device/essential/mata/vendor_prop.mk
 
+SRC_MEDIA_HAL_DIR := hardware/qcom/media/msm8998
+SRC_DISPLAY_HAL_DIR := hardware/qcom/display/msm8998
+SRC_CAMERA_HAL_DIR := hardware/qcom/camera/msm8998
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 560dpi
@@ -164,6 +168,9 @@ PRODUCT_PACKAGES += \
 # HDR
 PRODUCT_COPY_FILES += \
     device/essential/mata/configs/hdr_tm_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_tm_config.xml
+
+# Headers
+-include hardware/qcom/msm8998/msm8998.mk
 
 # IMS
 PRODUCT_PACKAGES += \
@@ -328,7 +335,8 @@ PRODUCT_PACKAGES += \
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     device/essential/mata \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/qcom/msm8998
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -376,6 +384,9 @@ PRODUCT_PACKAGES_DEBUG += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.mata
+
+# Build utilities
+-include device/essential/mata/utils.mk
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/system
