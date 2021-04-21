086afff830d6d3baf8ffcc872a418472dc4a7408
Anay Wadhera <awadhera@berkeley.edu>
2021-04-21 13:48:22 -0700

    mata: carbonate

--- carbon_mata.mk
+++ carbon_mata.mk
@@ -1,6 +1,7 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -15,12 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
-# Recovery
-TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
-
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := carbon_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
